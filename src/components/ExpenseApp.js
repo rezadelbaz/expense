783dc738f912d3fe87bb4e27139f700cad617a6e
reza delbaz <reza.delbaz74@gmail.com>
2021-09-11 02:54:31 +0430

    add Expense and Income Function

--- src/components/ExpenseApp.js
+++ src/components/ExpenseApp.js
@@ -1,16 +1,31 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import OverViewComponent from "./OverViewComponent";
 import TransActionComponent from "./TransActionComponent";
 
 const ExpensApp = () => {
   const [expence, setExpence] = useState(0);
   const [income, setIncome] = useState(0);
+
   const [transaction, setTransaction] = useState([]);
+
   const addTransAction = (formVlues) => {
     console.log(formVlues);
     const data = { ...formVlues, id: Date.now() };
     setTransaction([...transaction, data]);
   };
+
+  useEffect(() => {
+    let exp = 0;
+    let inc = 0;
+    transaction.forEach((t) => {
+      t.type === "expense"
+        ? (exp = exp + parseFloat(t.amount))
+        : (inc = inc + parseFloat(t.amount));
+      setExpence(exp);
+      setIncome(inc);
+    });
+  }, [transaction]);
+
   return (
     <section className="contianer">
       <OverViewComponent
