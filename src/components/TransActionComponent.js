c8d0229350345d78ebe658217554d20536fb6411
reza delbaz <reza.delbaz74@gmail.com>
2021-09-12 13:59:02 +0430

    add search feature

--- src/components/TransActionComponent.js
+++ src/components/TransActionComponent.js
@@ -1,15 +1,51 @@
+import { useEffect, useState } from "react";
+
 const TransActionComponent = ({ transaction }) => {
+  const [searchItem, setSearchItem] = useState("");
+  const [filterdTnx, setFilterdTnx] = useState(transaction);
+
+  const filterTransactions = (search) => {
+    if (!search || search === "") {
+      setFilterdTnx(transaction);
+      return;
+    }
+
+    const filterd = transaction.filter((t) =>
+      t.desc.toLowerCase().includes(search.toLowerCase())
+    );
+    setFilterdTnx(filterd);
+  };
+
+  useEffect(() => {
+    filterTransactions(searchItem);
+  }, [transaction]);
+
+  const changeHandler = (e) => {
+    setSearchItem(e.target.value);
+    filterTransactions(e.target.value);
+  };
+
+  if (!transaction.length) return <h3>add some transaction</h3>;
   return (
     <section>
-      {transaction.map((t) => (
-        <div
-          key={t.id}
-          className={`transaction ${t.type === "expense" && "expense"}`}
-        >
-          <span>{t.desc}</span>
-          <span>${t.amount}</span>
-        </div>
-      ))}
+      <input
+        type="text"
+        value={searchItem}
+        onChange={changeHandler}
+        placeholder="search items"
+        className="search"
+      />
+      {filterdTnx.length
+        ? filterdTnx.map((t) => (
+            <div
+              key={t.id}
+              className={`transaction ${t.type === "expense" && "expense"}`}
+            >
+              <span>{t.desc}</span>
+              <span>${t.amount}</span>
+            </div>
+          ))
+        : "item not match!"}
     </section>
   );
 };
